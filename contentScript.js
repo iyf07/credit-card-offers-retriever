0ce56ed6bba9e9e12b05bf020141858a0e3676b4
Ivan Yang <yfyang07@gmail.com>
2022-11-11 13:07:57 -0500

    Offers info are retrieved. Will be stored at localstorage

--- contentScript.js
+++ contentScript.js
@@ -5,10 +5,13 @@ const curUrl = window.location.toString();
 const amexEligible = "https://global.americanexpress.com/offers/eligible";
 const amexEnrolled = "https://global.americanexpress.com/offers/enrolled";
 
+
 // Check the current tab
 switch (curUrl) {
     case amexEligible:
         addAmexOffers()
+    case amexEnrolled:
+        getAmexOffers();
 }
 
 // Sleep function to wait
@@ -16,7 +19,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-// Add a button to the page and add event listener to wait for a click
+// Add a button to the page which activates all offers
 function addAmexOffers() {
     setTimeout(function () {
         const header = document.querySelector('.axp-offers__global__headerSection___1aNfL');
@@ -24,9 +27,9 @@ function addAmexOffers() {
         const offers = Array.from(allButtons).filter(button => button.title === "Add to Card" || button.title === "Activate Offer");
         var activateButton = addClickAmexOffersButton();
         header.appendChild(activateButton);
-        //if (offers.length == 0) {
-            //activateButton.setAttribute('disabled', true);
-        //}
+        if (offers.length == 0) {
+            activateButton.setAttribute('disabled', true);
+        }
         activateButton.addEventListener('click', clickAmexOffers);
     }, 3000)
 }
@@ -68,8 +71,78 @@ async function clickAmexOffers() {
     removeClickAmexOffersButton();
 }
 
+// Remove the button after activating all offers
 function removeClickAmexOffersButton() {
     var activateButton = document.getElementById('activateAmexOffers');
-    //const header = document.querySelector('.axp-offers__global__headerSection___1aNfL');
     activateButton.parentNode.removeChild(activateButton);
+}
+
+// Add a button to the page which retrieves all offers
+function getAmexOffers() {
+    setTimeout(function () {
+        const header = document.querySelector('.axp-offers__global__headerSection___1aNfL');
+        var retrieveButton = addRetrieveAmexOffersButton();
+        header.appendChild(retrieveButton);
+        retrieveButton.addEventListener('click', retrieveAmexOffers);
+    }, 3000)
+}
+
+// Add a button with styles to the amex offers page
+function addRetrieveAmexOffersButton() {
+    var retrieveButton = document.createElement('button');
+    retrieveButton.id = 'retrieveAmexOffers';
+    retrieveButton.className = 'col-sm-12 col-md-3 col-lg-2  css-ns91eb';
+    retrieveButton.innerHTML = 'Retrieve Offers';
+    retrieveButton.style.textAlign = "center";
+    retrieveButton.style.marginLeft = "30px";
+    return retrieveButton;
+}
+
+// Retrieve all offers info
+function retrieveAmexOffers() {
+    const accountNumber = document.getElementsByClassName('card-name heading-1 axp-account-switcher__accountSwitcher__lastFive___1s6L_ axp-account-switcher__accountSwitcher__hasOneCardLastFive___3mnDN');
+    const vendorNames = document.getElementsByClassName('body-1 margin-0-b dls-gray-05');
+    const offers = document.getElementsByClassName('heading-3 margin-0-b dls-gray-06');
+    const rawCloseExpirations = document.getElementsByClassName('strong label-2 dls-red');
+    const rawExpirations = document.getElementsByClassName('label-2 dls-gray-05');
+    const today = new Date();
+
+    for (let i = 0; i < vendorNames.length; i++) {
+        const vendor = vendorNames[i].textContent;
+        const offer = offers[i].textContent;
+        const account = accountNumber[0].textContent;
+        var expiration;
+        var expirationISO;
+
+        // Offers that expire soon
+        if (i < rawCloseExpirations.length) {
+            var closeExpiration = rawCloseExpirations[i].textContent;
+            var expirationDate = new Date();
+            switch (closeExpiration.length) {
+                case 13:
+                    break;
+                case 16:
+                    expirationDate.setDate(today.getDate() + 1);
+                    break;
+                case 17:
+                    expirationDate.setDate(today.getDate() + parseInt(closeExpiration[11]));
+                    break;
+                case 18:
+                    expirationDate.setDate(today.getDate() + parseInt(closeExpiration[12]) + parseInt(closeExpiration[11]) * 10);
+                    break;
+            }
+            expirationISO = expirationDate.toISOString();
+            var splits = expirationISO.split('-');
+            expiration = splits[1] + '/' + splits[2].slice(0, 2) + '/' + splits[0];
+
+        // offers that are not expiring in two weeks
+        } else {
+            expiration = rawExpirations[i - rawCloseExpirations.length].innerHTML;
+            var splits = expiration.split('/')
+            var expirationISODate = new Date(splits[2] + '-' + splits[0] + '-' + splits[1] + 'T00:00:00');
+            expirationISO = expirationISODate.toISOString();
+        }
+        var val = { 'offer': offer, 'expiration': expiration, 'account': account, 'expirationISO': expirationISO };
+        console.log(val)
+    }
 }
