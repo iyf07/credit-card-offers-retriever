bf1f29dfb8e7f03574cea63bac38b31869b18258
Ivan Yang <yfyang07@gmail.com>
2022-11-10 22:29:40 -0500

    Fixed bug. It will remove the activate button at the end each time

--- contentScript.js
+++ contentScript.js
@@ -8,7 +8,7 @@ const amexEnrolled = "https://global.americanexpress.com/offers/enrolled";
 // Check the current tab
 switch (curUrl) {
     case amexEligible:
-        amexOffers()
+        addAmexOffers()
 }
 
 // Sleep function to wait
@@ -17,29 +17,29 @@ function sleep(ms) {
 }
 
 // Add a button to the page and add event listener to wait for a click
-function amexOffers() {
+function addAmexOffers() {
     setTimeout(function () {
         const header = document.querySelector('.axp-offers__global__headerSection___1aNfL');
         const allButtons = document.getElementsByClassName("offer-cta");
         const offers = Array.from(allButtons).filter(button => button.title === "Add to Card" || button.title === "Activate Offer");
-        var activateButton = header.querySelector('button');
-        clickAmexOffersButton(activateButton);
+        var activateButton = addClickAmexOffersButton();
         header.appendChild(activateButton);
-        if (offers.length == 0) {
-            activateButton.setAttribute('disabled', true);
-        }
+        //if (offers.length == 0) {
+            //activateButton.setAttribute('disabled', true);
+        //}
         activateButton.addEventListener('click', clickAmexOffers);
     }, 3000)
 }
 
-// Add styles to the amex offers button
-function clickAmexOffersButton() {
-    activateButton = document.createElement('button');
+// Add a button with styles to the amex offers button
+function addClickAmexOffersButton() {
+    var activateButton = document.createElement('button');
     activateButton.id = 'activateAmexOffers';
-    activateButton.className = 'col - sm - 12 col - md - 3 col - lg - 2 offer - cta axp - offers__global__fluid___15Bk5 css - 1duq00z';
+    activateButton.className = 'col-sm-12 col-md-3 col-lg-2 offer-cta axp-offers__global__fluid___15Bk5 css-1duq00z';
     activateButton.innerHTML = 'Activate Offers';
     activateButton.style.textAlign = "center";
     activateButton.style.marginLeft = "30px";
+    return activateButton;
 }
 
 // Click the button to activate all offers. Will automatically reload
@@ -49,9 +49,7 @@ async function clickAmexOffers() {
     var emptyOffersTimes = 0;
     while (true) {
         const offers = Array.from(document.getElementsByClassName("offer-cta")).filter(btn => btn.title === "Add to Card" || btn.title === "Activate Offer");
-        console.log(offers.length)
         if (emptyOffersTimes == 2) {
-            alert(offersActivated + ' offers have been activated.')
             break
         }
         if (offers.length == 0) {
@@ -66,4 +64,12 @@ async function clickAmexOffers() {
         location.reload();
         await sleep(3000);
     }
+    alert(offersActivated + ' offers have been activated.')
+    removeClickAmexOffersButton();
+}
+
+function removeClickAmexOffersButton() {
+    var activateButton = document.getElementById('activateAmexOffers');
+    //const header = document.querySelector('.axp-offers__global__headerSection___1aNfL');
+    activateButton.parentNode.removeChild(activateButton);
 }
